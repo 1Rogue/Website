bf0ac7b3596cef7bb0dd2d6c95cb7348624d23b7
Spencer Alderman <spencer@1rogue.com>
2013-12-18 13:34:30 -0500

    init commit

---
+++ SampleRepo/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Spencer
+ *
+ * Created on December 18, 2013, 1:33 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
